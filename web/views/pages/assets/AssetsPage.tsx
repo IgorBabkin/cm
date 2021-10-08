0c96a963e6a71abf74a6fe6a0f433e6f5e8f95e3
Igor Babkin <igba14@gmail.com>
2021-10-08 17:43:05 +0200

    feat: add real persistence

--- web/views/pages/assets/AssetsPage.tsx
+++ web/views/pages/assets/AssetsPage.tsx
@@ -10,6 +10,7 @@ import cn from 'classnames';
 import { ResetFilters } from '../../../../application/useCases/assets/ResetFilters';
 import { MetricListFilterOptionsQuery } from '../../../../application/useCases/assets/MetricListFilterOptionsQuery';
 import { FilterMetricList } from '../../../../application/useCases/assets/FilterMetricList';
+import './assetsPage.scss';
 
 export const AssetsPage: FC = () => {
   const $ = useObservables();
@@ -28,58 +29,73 @@ export const AssetsPage: FC = () => {
   useEffect(() => loadAllList.execute(), []);
 
   return (
-    <div>
-      <button className="btn btn-danger" onClick={() => resetFilters.execute()}>
-        Reset
-      </button>
-      <div className="row mt-3">
-        <div className="col">
-          <div className="input-group rounded">
-            <input
-              type="search"
-              className="form-control rounded"
-              placeholder="Search"
-              value={$(assetListFilterOptions$)!.searchText}
-              onChange={({ target }) => filterAssetList.execute({ searchText: target.value })}
-            />
+    <div className="columns h-100">
+      <div className="column h-100">
+        <div className="panel h-100 flex-panel">
+          <div className="panel-heading">Assets</div>
+          <div className="panel-block">
+            <p className="control has-icons-left">
+              <input
+                className="input"
+                type="text"
+                placeholder="Search"
+                value={$(assetListFilterOptions$)!.searchText}
+                onChange={({ target }) => filterAssetList.execute({ searchText: target.value })}
+              />
+              <span className="icon is-left">
+                <i className="fas fa-search" aria-hidden="true" />
+              </span>
+            </p>
           </div>
-          <ul className="list-group">
-            <Each obs$={assetList$}>
-              {({ ticker, title }) => (
-                <li
-                  key={ticker}
-                  className={cn('list-group-item', { active: ticker === $(metricListFilterOptions$)?.ticker })}
-                  onClick={() => filterMetricList.execute({ ticker })}
-                >
-                  {title}
-                </li>
-              )}
-            </Each>
-          </ul>
+          <div className="panel-block grow-1 no-overflow">
+            <ul className="block-list w-100 h-100 auto-scroll">
+              <Each obs$={assetList$}>
+                {({ ticker, title }) => (
+                  <li
+                    key={ticker}
+                    className={cn({ 'is-highlighted': ticker === $(metricListFilterOptions$)?.ticker })}
+                    onClick={() => filterMetricList.execute({ ticker })}
+                  >
+                    {title}
+                  </li>
+                )}
+              </Each>
+            </ul>
+          </div>
         </div>
-        <div className="col">
-          <div className="input-group rounded">
-            <input
-              type="search"
-              className="form-control rounded"
-              placeholder="Search"
-              value={$(metricListFilterOptions$)!.searchText}
-              onChange={({ target }) => filterMetricList.execute({ searchText: target.value })}
-            />
+      </div>
+      <div className="column h-100">
+        <div className="panel h-100 flex-panel">
+          <div className="panel-heading">Metrics</div>
+          <div className="panel-block">
+            <p className="control has-icons-left">
+              <input
+                className="input"
+                type="text"
+                placeholder="Search"
+                value={$(metricListFilterOptions$)!.searchText}
+                onChange={({ target }) => filterMetricList.execute({ searchText: target.value })}
+              />
+              <span className="icon is-left">
+                <i className="fas fa-search" aria-hidden="true" />
+              </span>
+            </p>
           </div>
-          <ul className="list-group">
-            <Each obs$={metricList$}>
-              {({ name, title }) => (
-                <li
-                  key={name}
-                  className={cn('list-group-item', { active: name === $(assetListFilterOptions$)?.metric })}
-                  onClick={() => filterAssetList.execute({ metric: name })}
-                >
-                  {title}
-                </li>
-              )}
-            </Each>
-          </ul>
+          <div className="panel-block grow-1 no-overflow">
+            <ul className="block-list w-100 h-100 auto-scroll">
+              <Each obs$={metricList$}>
+                {({ name, title }) => (
+                  <li
+                    key={name}
+                    className={cn({ 'is-highlighted': name === $(assetListFilterOptions$)?.metric })}
+                    onClick={() => filterAssetList.execute({ metric: name })}
+                  >
+                    {title}
+                  </li>
+                )}
+              </Each>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
