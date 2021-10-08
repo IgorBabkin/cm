d432d8af62cf562f02886129796c6965e1faeb3d
Igor Babkin <igba14@gmail.com>
2021-10-08 18:57:38 +0200

    refactor: cleanup

--- web/ui/Panel.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { FC } from 'react';
-import './Panel.scss';
-
-export const Panel: FC = ({ children }) => {
-  return <div className="panel">{children}</div>;
-};
